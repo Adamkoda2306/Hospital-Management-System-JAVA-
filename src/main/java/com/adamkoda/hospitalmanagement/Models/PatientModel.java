1bef62824296332efcd712a4a6107a2ff1cdd037
AdamKoda2306 <adam.koda2306@gmail.com>
2026-05-09 17:04:18 +0530

    codes till now is getting pushed

--- src/main/java/com/adamkoda/hospitalmanagement/Models/PatientModel.java
+++ src/main/java/com/adamkoda/hospitalmanagement/Models/PatientModel.java
@@ -1,5 +1,58 @@
 package com.adamkoda.hospitalmanagement.Models;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.adamkoda.hospitalmanagement.Utils.DatabaseUtils;
+
 public class PatientModel {
+    private String id;
+    private String name;
+    private String doctorId;
+    private List<Date> previousCheckups;
+
+    public PatientModel(String name, String doctorId) {
+        this.id = DatabaseUtils.generateId("PAT");
+        this.name = name;
+        this.doctorId = doctorId;
+        this.previousCheckups = new ArrayList<>();
+    }
+
+    public PatientModel(String id, String name, String doctorId, List<Date> previousCheckups) {
+        this.id = id;
+        this.name = name;
+        this.doctorId = doctorId;
+        this.previousCheckups = previousCheckups;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
     
+    public String getdoctorId() {
+        return doctorId;
+    }
+
+    public List<Date> getPreviousCheckups() {
+        return previousCheckups;
+    }
+
+    // Setters
+    public void setName(String Name) {
+        this.name = Name;
+    }
+
+    public void setDoctorId(String Id) {
+        this.doctorId = Id;
+    }
+
+    public void setPreviousCheckupDate(Date previous) {
+        this.previousCheckups.add(previous);
+    }
 }
