84896442679ad76d32837d230b4a7c0ab19857ea
AdamKoda2306 <adam.koda2306@gmail.com>
2026-05-12 20:26:47 +0530

    added the doctor and patients dashboards

--- src/main/java/com/adamkoda/hospitalmanagement/Services/Doctors.java
+++ src/main/java/com/adamkoda/hospitalmanagement/Services/Doctors.java
@@ -1,8 +1,94 @@
 package com.adamkoda.hospitalmanagement.Services;
 
+import java.util.List;
+import java.util.Scanner;
+
+import com.adamkoda.hospitalmanagement.DatabaseOperations.DoctorDAO;
+import com.adamkoda.hospitalmanagement.Models.DoctorModel;
+import com.adamkoda.hospitalmanagement.Utils.GeneralUtils;
+
 public class Doctors {
-    /*
-        1) check Doctors
-        2) view Doctors
-    */
+   private Scanner scanner;
+   private DoctorDAO databaseOperations;
+
+   public Doctors(Scanner scanner) {
+    this.scanner = scanner;
+    databaseOperations = new DoctorDAO();
+   }    
+
+   private void displayDoctorDetails(DoctorModel data, boolean multiple) {
+    System.out.println("*****************************************************");
+    System.out.println("Doctor's Id: " + data.getId());
+    System.out.println("Doctor's Name: " + data.getName());
+    System.out.println("Speciality: " + data.getSpeciality());
+    System.out.println("                                                      ----------------------------");
+    System.out.println("                                                      |   "+ (data.getIsAvailable() ? "    Available" : "Not Available") + "  |");
+    System.out.println("                                                      ----------------------------");
+    System.out.println("----------------------------");
+    System.out.println("|   "+ (data.getIsResgined() ? "    Resigned" : "Not Resigned") + "  |");
+    System.out.println("----------------------------");
+    if (!multiple) {
+        System.out.println("Patient's List: ");
+        for (String patient: (data.getPatientsList()).keySet()) {
+            System.out.println("Patient Id: " + patient);
+            System.out.println("Patient's Name: " + (data.getPatientsList()).get(patient)+"\n");
+        }
+        GeneralUtils.pause();
+    }
+   }
+
+   private void displayDoctorAvailability(DoctorModel data) {
+    System.out.println("*****************************************************");
+    System.out.println("Doctor's Id: " + data.getId());
+    System.out.println("Doctor's Name: " + data.getName());
+    System.out.println("Speciality: " + data.getSpeciality());
+    System.out.println("                                                      ----------------------------");
+    System.out.println("                                                      |   "+ (data.getIsAvailable() ? "    Available" : "Not Available") + "  |");
+    System.out.println("                                                      ----------------------------");
+    GeneralUtils.pause();
+   }
+
+   public void addDoctor() {
+    System.out.println("ADDING NEW DOCTOR");
+    System.out.println("*****************************************************\n");
+    System.out.print("Enter Doctor's Name: ");
+    String name = scanner.next();
+    System.out.print("Enter Doctor's Speciality: ");
+    String speciality = scanner.next();
+    DoctorModel data = new DoctorModel(name, speciality);
+    databaseOperations.addDoctor(data);
+    GeneralUtils.clearConsole();
+    System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+    System.out.println("|           Successfully Added the Doctor!!            |");
+    System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+    GeneralUtils.pause();
+   }
+
+   public void checkDoctor() {
+    System.out.print("Enter the Doctor's Id: ");
+    String id = scanner.next();
+    GeneralUtils.clearConsole();
+    DoctorModel doctorData = databaseOperations.getDoctorDetails(id);
+    displayDoctorDetails(doctorData, false);
+   }
+
+   public void checkDoctorAvailability() {
+    System.out.print("Enter the Doctor's Id: ");
+    String id = scanner.next();
+    GeneralUtils.clearConsole();
+    DoctorModel doctorData = databaseOperations.getDoctorDetails(id);
+    displayDoctorAvailability(doctorData);
+   }
+
+   public void listDoctors() {
+    GeneralUtils.clearConsole();
+    List<DoctorModel> Data = databaseOperations.getAllDoctorDetails();
+    System.out.println("=====================================================");
+    System.out.println("Doctor's List : ");
+    System.out.println("=====================================================");
+    for (DoctorModel data: Data) {
+        displayDoctorDetails(data, true);
+    }
+    GeneralUtils.pause();
+   }
 }
--- src/main/java/com/adamkoda/hospitalmanagement/Services/Patients.java
+++ src/main/java/com/adamkoda/hospitalmanagement/Services/Patients.java
@@ -1,9 +1,67 @@
 package com.adamkoda.hospitalmanagement.Services;
 
+import java.util.List;
+import java.util.Scanner;
+
+import com.adamkoda.hospitalmanagement.DatabaseOperations.PatientsDAO;
+import com.adamkoda.hospitalmanagement.Models.PatientModel;
+import com.adamkoda.hospitalmanagement.Utils.GeneralUtils;
+
 public class Patients {
-    /* 
-        1) add Patients
-        2) view Patients
-        3) check Patients
-    */
+   private Scanner scanner;
+   private PatientsDAO databaseOperations;
+
+   public Patients(Scanner scanner) {
+    this.scanner = scanner;
+    databaseOperations = new PatientsDAO();
+   }
+
+   private void displayPatientDetails(PatientModel data, boolean multiple) {
+    System.out.println("*****************************************************");
+    System.out.println("Patient's Id: " + data.getId());
+    System.out.println("Patient's Name: " + data.getName());
+    if (!multiple) {
+        System.out.println("Previous Checkups: ");
+        for (String date : data.getPreviousCheckups()) {
+            System.out.println(date);
+        }
+        GeneralUtils.pause();
+    }
+   }
+
+   public void addPatients() {
+    System.out.println("ADDING NEW PATIENT");
+    System.out.println("*****************************************************\n");
+    System.out.print("Enter Patient's Name: ");
+    String name = scanner.next();
+    System.out.print("Enter Doctor's Id: ");
+    String docId = scanner.next();
+    PatientModel data = new PatientModel(name, docId);
+    databaseOperations.addPatient(data);
+    GeneralUtils.clearConsole();
+    System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+    System.out.println("|           Successfully Added the Patient!!           |");
+    System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+    GeneralUtils.pause();
+   }
+
+   public void checkPatient() {
+    System.out.print("Enter the Patient's Id: ");
+    String id = scanner.next();
+    GeneralUtils.clearConsole();
+    PatientModel patientData = databaseOperations.getPatientDetails(id);
+    displayPatientDetails(patientData, false);
+   }
+
+   public void listPatients() {
+    GeneralUtils.clearConsole();
+    List<PatientModel> patientData = databaseOperations.getAllPatientDetails();
+    System.out.println("=====================================================");
+    System.out.println("Patient's List : ");
+    System.out.println("=====================================================");
+    for (PatientModel data: patientData) {
+        displayPatientDetails(data, true);
+    }
+    GeneralUtils.pause();
+   }
 }
