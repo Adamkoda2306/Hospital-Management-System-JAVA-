b4ff3d597fd2ab97346ef161dfddf1c8d76c37f2
AdamKoda2306 <adam.koda2306@gmail.com>
2026-06-07 18:32:25 +0530

    added diplay appointment details function in the appointment service

--- src/main/java/com/adamkoda/hospitalmanagement/Services/Appointments.java
+++ src/main/java/com/adamkoda/hospitalmanagement/Services/Appointments.java
@@ -3,6 +3,12 @@ package com.adamkoda.hospitalmanagement.Services;
 import java.util.Scanner;
 
 import com.adamkoda.hospitalmanagement.DatabaseOperations.AppointmentsDAO;
+import com.adamkoda.hospitalmanagement.DatabaseOperations.DoctorDAO;
+import com.adamkoda.hospitalmanagement.DatabaseOperations.PatientsDAO;
+import com.adamkoda.hospitalmanagement.Models.AppointmentModel;
+import com.adamkoda.hospitalmanagement.Models.DoctorModel;
+import com.adamkoda.hospitalmanagement.Models.PatientModel;
+import com.adamkoda.hospitalmanagement.Utils.GeneralUtils;
 
 public class Appointments {
     /*
@@ -12,9 +18,29 @@ public class Appointments {
     */
    private Scanner scanner;
    private AppointmentsDAO databaseOperations;
+   private DoctorDAO doctor;
+   private PatientsDAO patient;
 
    public Appointments(Scanner scanner) {
     this.scanner = scanner;
     databaseOperations = new AppointmentsDAO();
+    doctor = new DoctorDAO();
+    patient =  new PatientsDAO();
    }
+
+   private void displayAppointmentDetails(AppointmentModel data, boolean multiple) {
+    DoctorModel doctorData = doctor.getDoctorDetails(data.getDoctorId());
+    PatientModel patientData = patient.getPatientDetails(data.getPatientId());
+    System.out.println("*****************************************************");
+    System.out.println("Appointment's Id: " + data.getId());
+    System.out.println("Doctor's Name: " + doctorData.getName());
+    System.out.println("Patient's Name: " + patientData.getName());
+    System.out.println("Appointment Date: " + data.getAppointmentDate());
+    String consulted = data.getIsConsulted() ? "  DONE  " : "NOT DONE";
+    System.out.println("-----------------     ");
+    System.out.println("| " + consulted + " |     | " );
+    System.out.println("-----------------     ");
+    GeneralUtils.pause();
+   }
+
 }
